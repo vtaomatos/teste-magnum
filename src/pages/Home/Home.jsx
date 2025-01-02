d7eef8c9b6414a77a84c66eb288abd5974bfa310
Vitor Matos <vamatos48@hotmail.com>
2025-01-02 17:31:21 -0300

    Adiciona mensagem personalizada de bem vindo ao usuario com seu nome na tela dashboard.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,28 @@
+import { useState, useEffect } from "react";
 
 function Home() {
-    return "é Home";
+  const [primeiroNome, setPrimeiroNome] = useState("");
+
+  useEffect(() => {
+    const contaString = localStorage.getItem("conta");
+
+    if (contaString) {
+      try {
+        const conta = JSON.parse(contaString);
+        if (conta && conta.primeiroNome) {
+          setPrimeiroNome(conta.primeiroNome);
+        }
+      } catch (error) {
+        console.error("Erro ao parsear o valor do localStorage:", error);
+      }
+    }
+  }, []);
+
+  return (
+    <div>
+      <h1>Bem-Vindo(a), {primeiroNome}!</h1>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
